5bf487e732a505cd60d7ce771df5299eca401aef
paeulaa <56285615+paeulaa@users.noreply.github.com>
2020-06-02 20:18:42 +0800

    use bootstrap template

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Navigation from './components/Navigation'
+import Masthead from './components/Masthead'
+import Portfolio from './components/Portfolio'
+import About from './components/About'
+import Contact from './components/Contact'
+import Call_to_action from './components/Contact'
+import Services from './components/Contact'
+import Footer from './components/Footer'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navigation/>
+        <Masthead/>
+        <Portfolio/>
+        <About/>
+        <Contact/>
+        <Call_to_action/>
+        <Services/>
+        <Footer/>
+      </div>
+    );
+  }
 }
 
 export default App;
